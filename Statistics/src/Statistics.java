01d72187b508a4860cf54805174fc364c00010d3
Devona Spruell <devona_spruell@dell.com>
2019-01-16 23:04:26 -0600

    Statistics Project with Updates

--- Statistics/src/Statistics.java
+++ Statistics/src/Statistics.java
@@ -5,7 +5,7 @@ public class Statistics {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Please enter a list of numbers in separated by commas.");
+		System.out.println("Please enter a list of numbers separated by commas.");
 		Scanner reader = new Scanner(System.in);
 		String numbersString = reader.nextLine();
 		
@@ -19,45 +19,38 @@ public class Statistics {
 		minimum(numbersInt);
 		maximum(numbersInt);	
 		sum(numbersInt);
-//		minimum(); //function needs to find the min of the list of numbers
-//		maximum(); //function needs to find the max of the list of numbers
-//		amountOfNumbers(); //function needs to find the amount of numbers received from the user
-//		average(); //function needs to find the average of the list of numbers
-//		sum();	//function needs to find the sum of all numbers
 	}
-	
-	public static void minimum(Integer[] numbersArray) { 
+	public static void minimum(Integer[] numbersArray) {
 		int currentMin = numbersArray[0];
 		for (int i = 0; i < numbersArray.length; i++) {
-			if	(numbersArray[i] < currentMin) {
+			if (numbersArray[i] < currentMin) {
 				currentMin = numbersArray[i];
-			} 
-		} System.out.println("Minimum number is " + currentMin + "");
-	} 
-	public static void maximum(Integer[] numbersArray) { 
+			}
+		}
+		System.out.println("Minimum number is " + currentMin + "");
+	}
+
+	public static void maximum(Integer[] numbersArray) {
 		int currentMax = numbersArray[0];
 		for (int i = 0; i < numbersArray.length; i++) {
-			if	(numbersArray[i] > currentMax) {
+			if (numbersArray[i] > currentMax) {
 				currentMax = numbersArray[i];
-			} 
-		} System.out.println("Maximum number is " + currentMax + "");
+			}
+		}
+		System.out.println("Maximum number is " + currentMax + "");
 		System.out.println("Amount of numbers submitted is " + numbersArray.length + "");
-	} 
-	public static void sum(Integer[] numbersArray) { 
+	}
+
+	public static void sum(Integer[] numbersArray) {
 		int sum = 0;
-		
+
 		for (int j = 0; j < numbersArray.length; j++) {
 			sum += numbersArray[j];
-			} 
-		 System.out.println("Sum of all numbers submitted is " + sum + "");
-		 int average = sum / numbersArray.length;
-		 System.out.println("Average of all numbers submitted is " + average + "");
+		}
+		System.out.println("Sum of all numbers submitted is " + sum + "");
+		int averageOne = sum / numbersArray.length;
+		String averageTwo = Integer.toString(averageOne);
+		double averageFinal = Double.parseDouble(averageTwo);
+		System.out.println("Average of all numbers submitted is " + averageFinal + "");
 	}
-}	
-	
-
-
-		
-		
-	
-
+}
