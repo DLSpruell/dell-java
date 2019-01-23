e9986f1f785965ec9ca7e52f5fc3cb3dfd6dafcd
Devona Spruell <devona_spruell@dell.com>
2019-01-23 13:23:03 -0600

    Statistics with Updated code and comments

--- Statistics/src/Statistics.java
+++ Statistics/src/Statistics.java
@@ -8,7 +8,6 @@ public class Statistics {
 		System.out.println("Please enter a list of numbers separated by commas.");
 		Scanner reader = new Scanner(System.in);
 		String numbersString = reader.nextLine();
-		
 		reader.close();
 		String[] numbersArray = numbersString.split(","); //returns the list split up by commas//rename to make sense
 		Integer[] numbersInt = new Integer[numbersArray.length];      //returns each item in the list as an integer
@@ -16,41 +15,41 @@ public class Statistics {
 			Integer num = Integer.parseInt(numbersArray[i]); 
 			numbersInt[i] = num;
 		}
-		minimum(numbersInt);
-		maximum(numbersInt);	
-		sum(numbersInt);
+		minimum(numbersInt); //helper code to find the smallest number in the list
+		maximum(numbersInt);	//helper code to find the largest number in the list and the amount of numbers in the list
+		sum(numbersInt); //helper code to find the sum and average of all numbers in the list
 	}
 	public static void minimum(Integer[] numbersArray) {
-		int currentMin = numbersArray[0];
+		int currentMin = numbersArray[0]; 
 		for (int i = 0; i < numbersArray.length; i++) {
 			if (numbersArray[i] < currentMin) {
-				currentMin = numbersArray[i];
+				currentMin = numbersArray[i]; // code to find the smallest number is the list
 			}
 		}
-		System.out.println("Minimum number is " + currentMin + "");
+		System.out.println("Minimum number is " + currentMin + ""); //print out the result 
 	}
 
 	public static void maximum(Integer[] numbersArray) {
 		int currentMax = numbersArray[0];
 		for (int i = 0; i < numbersArray.length; i++) {
 			if (numbersArray[i] > currentMax) {
-				currentMax = numbersArray[i];
+				currentMax = numbersArray[i]; //find the largest number in the list
 			}
 		}
-		System.out.println("Maximum number is " + currentMax + "");
-		System.out.println("Amount of numbers submitted is " + numbersArray.length + "");
+		System.out.println("Maximum number is " + currentMax + ""); //print out the result of the larget number
+		System.out.println("Amount of numbers submitted is " + numbersArray.length + ""); //find and print out the number of elements in the list
 	}
 
 	public static void sum(Integer[] numbersArray) {
 		int sum = 0;
 
 		for (int j = 0; j < numbersArray.length; j++) {
-			sum += numbersArray[j];
+			sum += numbersArray[j]; //add up all of the numbers in the list
 		}
-		System.out.println("Sum of all numbers submitted is " + sum + "");
-		int averageOne = sum / numbersArray.length;
-		String averageTwo = Integer.toString(averageOne);
-		double averageFinal = Double.parseDouble(averageTwo);
-		System.out.println("Average of all numbers submitted is " + averageFinal + "");
+		System.out.println("Sum of all numbers submitted is " + sum + ""); //print out the number of elements in the list
+		double sumFinal = (double) sum;
+		double averageOne = (sumFinal / numbersArray.length); //divide the sum of the list by the amount of numbers in the list to get the average
+		
+		System.out.println("Average of all numbers submitted is " + averageOne + "");//print out the result
 	}
 }
